1221d3a2fb64c959c65114102a3e0dc631c59aaa
Ryan Ramage <ryan.ramage@gmail.com>
2012-01-10 22:22:37 -0700

    A better way to mock the emit function.

--- test/nodeunit/module_app_test.js
+++ test/nodeunit/module_app_test.js
@@ -1,14 +1,33 @@
 
 var app = require('../../lib/app');
 
-exports.testSomething = function(test){
-
-    var doc = {
-        make : 'Honda'
-    }
-    app.views.makes.map(doc);
-    test.equal(1, app.views.results.length, "Doc emitted");
-    test.equal("Honda", app.views.results[0].key, "Correct key");
-
-    test.done();
+var results;
+global.emit = function(key, value) {
+    results.push({key : key, value : value});
 };
+
+module.exports = {
+    setUp : function(callback) {
+        results = [];
+        callback();
+    },    
+    testDocWithMake : function(test){
+       
+        var doc = {
+            make : 'Honda'
+        }
+        app.views.makes.map(doc);
+        test.equal(1, results.length, "Doc emitted");
+        test.equal("Honda", results[0].key, "Correct key");
+        test.done();
+    },
+    testDocWithNoMake : function(test){
+
+        var doc = {};
+        app.views.makes.map(doc);
+        test.equal(0, results.length, "No Doc emitted");
+        test.done();
+    }
+    
+}
+    
