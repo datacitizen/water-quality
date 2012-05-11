5a77266e372f1590521ac6713314b61405f72d31
Ryan Ramage <ryan.ramage@gmail.com>
2012-05-11 11:06:27 -0600

    Import buttons to load available source data.

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,16 +1,56 @@
+var db = require('db').use('_db');
+var _  = require('underscore')._;
+
 $(function(){
 
-    var public_spreadsheet_url = '0AhQVbLcvyJvIdEEtX0dkOG1Xd0lpUXNualEwYVVFbEE';
 
-    Tabletop.init( { key: public_spreadsheet_url,
-                     callback: showInfo,
-                     simpleSheet: false,
-                     wanted: [ "Salt River Data", "Buffalo River Data", "L Buffalo River ", "Kakisa River Data", "Hay River West Channel", "Slave Mouth Data" ]
-        } )
 
-    function showInfo(data, tabletop) {
+    $('.fort_smith').click(function(){
+        $(this).attr('disabled', 'disabled');
+        Tabletop({
+            key: '0AhQVbLcvyJvIdGgwVFVLUWZUanRaaHlRb3pDempSVnc',
+            simpleSheet: false,
+            wanted: [ "wqi_sample_value" ],
+            callback : function(data, tabletop) {
+                var data = _.map(data.wqi_sample_value.elements, function(row){
+                    row.source = 'wqi_sample_value';
+                    row.import_date = new Date().getTime();
+                    return row;
+                });
+                db.bulkSave(data, function(err, response) {
+                   if (err) console.log('error:', err);
+                });
+            }
+        });
+    });
 
-      console.log(data);
-    }
+
+    $('.north_sites').click(function(){
+        $(this).attr('disabled', 'disabled');
+        Tabletop({
+            key: '0AhQVbLcvyJvIdEEtX0dkOG1Xd0lpUXNualEwYVVFbEE',
+            simpleSheet: false,
+            wanted: [ "Salt River Data", "Buffalo River Data", "L Buffalo River ", "Kakisa River Data", "Hay River West Channel", "Slave Mouth Data" ],
+            callback : function(data, tabletop) {
+
+                var sources = _.keys(data);
+                _.each(sources, function(source) {
+                    var data_annotated = _.map(data[source].elements, function(row){
+                        row.source = source;
+                        row.import_date = new Date().getTime();
+                        return row;
+                    });
+
+                    db.bulkSave(data_annotated, function(err, response) {
+                       if (err) console.log('error:', err);
+                    });
+                });
+
+
+            }
+        });
+
+    });
+
 
 })
